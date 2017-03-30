c3c034b02bb71791789827708c4e56062b735a51
Cao Ming <cm9999@qq.com>
2017-03-30 23:15:56 +0800

    添加项目文件。

---
+++ Transparent/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Transparent
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelHourAndMinute = new System.Windows.Forms.Label();
+            this.labelSecond = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelHourAndMinute
+            // 
+            this.labelHourAndMinute.AutoSize = true;
+            this.labelHourAndMinute.BackColor = System.Drawing.Color.Black;
+            this.labelHourAndMinute.Font = new System.Drawing.Font("Letter Gothic Std", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHourAndMinute.ForeColor = System.Drawing.Color.White;
+            this.labelHourAndMinute.Location = new System.Drawing.Point(87, 219);
+            this.labelHourAndMinute.Name = "labelHourAndMinute";
+            this.labelHourAndMinute.Size = new System.Drawing.Size(340, 117);
+            this.labelHourAndMinute.TabIndex = 0;
+            this.labelHourAndMinute.Text = "00:00";
+            // 
+            // labelSecond
+            // 
+            this.labelSecond.AutoSize = true;
+            this.labelSecond.BackColor = System.Drawing.Color.Black;
+            this.labelSecond.Font = new System.Drawing.Font("Letter Gothic Std", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSecond.ForeColor = System.Drawing.Color.White;
+            this.labelSecond.Location = new System.Drawing.Point(433, 279);
+            this.labelSecond.Name = "labelSecond";
+            this.labelSecond.Size = new System.Drawing.Size(109, 78);
+            this.labelSecond.TabIndex = 1;
+            this.labelSecond.Text = "00";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.labelSecond);
+            this.Controls.Add(this.labelHourAndMinute);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHourAndMinute;
+        private System.Windows.Forms.Label labelSecond;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Transparent/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Transparent
+{
+    public partial class MainForm : Form
+    {
+        private readonly Graphics dc;
+        private readonly Graphics dc_this;
+        private Pen penCircle = new Pen(Color.White);
+        private readonly Bitmap bmp = new Bitmap(600, 600);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            dc = Graphics.FromImage(bmp);
+            dc_this = this.CreateGraphics();
+            penCircle.DashStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            penCircle.DashPattern = new float[] { 5, 10 };
+            this.Location = new Point(660,240);
+            dc.DrawEllipse(penCircle, 30, 30, 540, 540);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            this.TransparencyKey = Color.Black;
+            timer.Start();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            dc_this.DrawImage(bmp, 0, 0);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int hour = DateTime.Now.Hour;
+            int minute = DateTime.Now.Minute;
+            int second = DateTime.Now.Second;
+            labelHourAndMinute.Text = (hour >=10 ? hour.ToString() : '0' + hour.ToString() )+ ":" +
+                (minute >= 10 ? minute.ToString() : '0' + minute.ToString());
+            labelSecond.Text = second >= 10 ? second.ToString() :'0'+second.ToString();
+        }
+    }
+}
